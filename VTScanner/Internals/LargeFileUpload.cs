c4e7a223f43b330555381f5d124d428c5f73f18f
Muhammad Nabeel <61131901+ZeroLogic01@users.noreply.github.com>
2020-04-18 08:14:52 +0500

    Create LargeFileUpload.cs

---
+++ VTScanner/Internals/LargeFileUpload.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace VTScanner.Internals
+{
+    internal class LargeFileUpload
+    {
+        [JsonProperty("data")]
+        public string UploadUrl { get; set; }
+    }
+}
