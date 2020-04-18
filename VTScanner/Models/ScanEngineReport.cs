2cca9d96378c6daa52f979f68b41bebfa96a104a
Muhammad Nabeel <61131901+ZeroLogic01@users.noreply.github.com>
2020-04-18 21:51:21 +0500

    Added a custom template for the scan engine reports

---
+++ VTScanner/Models/ScanEngineReport.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace VTScanner.Models
+{
+    public class ScanEngineReport
+    {
+        [JsonProperty("file-name")]
+        public string FileName { get; set; }
+
+        [JsonProperty("engine_name")]
+        public string EngineName { get; set; }
+
+        [JsonProperty("is-detected")]
+        public int IsDetected { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+    }
+}
