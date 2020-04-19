2a74afe6265f573e40d19118edce38687323a295
Muhammad Nabeel <61131901+ZeroLogic01@users.noreply.github.com>
2020-04-19 09:27:51 +0500

    Added try catch block

--- VirusTotalScannerApp/Program.cs
+++ VirusTotalScannerApp/Program.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using VTScanner;
 using VTScanner.Results;
+using VTScanner.Results.AnalysisResults;
 
 namespace VirusTotalScannerApp
 {
@@ -13,20 +15,46 @@ namespace VirusTotalScannerApp
         static async Task Main(string[] args)
         {
 #if DEBUG
-            args = new[] { @"F:\machine_learning.pdf", "Key.txt" };
+            args = new[] { @"", "Key.txt" };
 #endif
-            string apiKeyFileName = args[1];
-            string fileToScan = args[0];
-
-            Scanner scanner = new Scanner(apiKeyFileName) { UseTLS = true };
-            var result = await scanner.ScanFileAsync(fileToScan).ConfigureAwait(false);
-
-            if (result != null)
+            Scanner scanner = null;
+            try
             {
+                string apiKeyFileName = args[1];
+                string fileToScan = args[0];
 
+                scanner = new Scanner(apiKeyFileName) { UseTLS = true, Timeout = TimeSpan.FromMilliseconds(Timeout.Infinite) };
+                scanner.OnProgressChanged += Scanner_OnProgressMade;
+                await scanner.ScanFileAndGenrateOutputAsync(fileToScan).ConfigureAwait(false);
             }
-
+            catch (Exception ex)
+            {
+                StringBuilder exceptionText = new StringBuilder();
+                exceptionText.Append(ex.Message);
+                while (ex.InnerException != null)
+                {
+                    exceptionText.Append($" {ex.InnerException.Message}");
+                    ex = ex.InnerException;
+                }
+                Console.Error.WriteLine(exceptionText.ToString());
+            }
+            finally
+            {
+                if (scanner != null)
+                {
+                    scanner.OnProgressChanged -= Scanner_OnProgressMade;
+                    scanner.Dispose();
+                }
+            }
+            Console.WriteLine("Press enter to exit...");
             Console.ReadLine();
         }
+
+        private static void Scanner_OnProgressMade(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+
     }
 }
